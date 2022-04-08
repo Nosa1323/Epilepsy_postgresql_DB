5ca1d691f40033acf507832bdf7931ad94983e4c
Nosa1323 <manekyou.uchiha@gmail.com>
2022-04-08 19:49:40 +0300

    1.1
    добавила связи один-ко-многим

--- sql_script_create_db.sql
+++ sql_script_create_db.sql
@@ -1,12 +1,26 @@
+drop table info;
+drop table gfap;
+
+create table demo_schema.info (
+	id SERIAL PRIMARY KEY, 
+    animal_num INT UNIQUE,
+    exp_group VARCHAR(10) UNIQUE,
+    cell_num INT UNIQUE,
+	protein VARCHAR(10) UNIQUE
+);
+
 create table demo_schema.gfap (
 	id SERIAL PRIMARY KEY, 
+	animal_num INT NOT NULL REFERENCES info (animal_num),
+    exp_group VARCHAR(10) NOT NULL REFERENCES info (exp_group),
+    cell_num INT NOT NULL REFERENCES info (cell_num),
+	protein VARCHAR(10) NOT NULL REFERENCES info (protein),
     volume DECIMAL NOT NULL,
     surface_area DECIMAL NOT NULL,
-    animal_num INT NOT NULL,
-    exp_group VARCHAR(10) NOT NULL,
-    cell_num INT NOT NULL,
-	protein VARCHAR(10) NOT NULL
-	);
+	FOREIGN KEY (id) 
+	REFERENCES info(id) );
+	
+	
 create table demo_schema.gs (
 	id SERIAL PRIMARY KEY, 
     volume DECIMAL NOT NULL,
@@ -46,25 +60,14 @@ create table demo_schema.cx43 (
     cell_num INT NOT NULL,
 	protein VARCHAR(10) NOT NULL
 	);
-create table demo_schema.info (
-	id SERIAL PRIMARY KEY, 
-    animal_num INT NOT NULL,
-    exp_group VARCHAR(10) NOT NULL,
-    cell_num INT NOT NULL,
-	protein VARCHAR(10) NOT NULL,
-	FOREIGN KEY (id) REFERENCES gfap(id),
-	FOREIGN KEY (id) REFERENCES gs(id),
-	FOREIGN KEY (id) REFERENCES glt(id),
-	FOREIGN KEY (id) REFERENCES s100b(id),
-	FOREIGN KEY (id) REFERENCES cx43(id)
-);
 
 
+drop table info;
+drop table gfap;
 drop table gs;
 drop table cx43;
-drop table info;
 drop table glt;
 drop table s100b;
-drop table gfap;
+
 
 select * from info
